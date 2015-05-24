6f95cd4713c0fbeb3b3ac8a90b1dccb29ffdd268
Nathan <brain.nathan@gmail.com>
2015-05-24 16:08:38 -0700

    Added comments to code per Study drill 1

--- ex3.py
+++ ex3.py
@@ -1,23 +1,33 @@
+# Prints informational text
 print "I will now count my chickens:"
 
+# Calculates amount of hens
 print "Hens", 25 + 30 / 6
+# Calculates amount of roosters
 print "Roosters", 100 - 25 * 3 % 4
 
+# Prints informational text
 print "Now I will count the eggs:"
-
+# Calculates egg amount
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# Asks a question of the user
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# Evaluates equation for True/False
 print 3 + 2 < 5 - 7
 
+# Asks question and answers (both lines)
 print "What is 3 + 2?", 3 + 2
 print "What is 5-7?", 5-7
 
+# Prints obvious answer
 print "Oh, that's why it's False."
 
+# Asks rhetorical question
 print "How about some more."
 
+# Asks a few more easy questions and answers them for you
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2
