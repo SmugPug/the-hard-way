26ba2dab4ed0f30e3aa613a4e035a17106dab62f
Nathan <brain.nathan@gmail.com>
2015-05-24 16:15:51 -0700

    Drill #3 - create simple math calculation

---
+++ ex3.sd3.py
@@ -0,0 +1,9 @@
+# Calculate area of rectangle
+
+print "We are going to calculate the area of a rectangle."
+
+print "Rectangle width = 10, length = 15"
+
+print "Area of rectangle = Width * Length"
+
+print "10 * 15 = ", 10*15
