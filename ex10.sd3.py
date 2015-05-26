e688c8d19258e07f554eee81d44e794e36af7996
Nathan <brain.nathan@gmail.com>
2015-05-26 02:40:04 -0700

    Self practice on escape sequences and formats

---
+++ ex10.sd3.py
@@ -0,0 +1,8 @@
+fat_cat = "Check out this cool thing: %s" % "\rYup"
+
+print fat_cat
+
+print "Check out how fast I can count to 10"
+while True:
+	for i in ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']:
+		print "%s\r" % i,
