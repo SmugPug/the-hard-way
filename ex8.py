42ac2a675ff57fc1b6903a57ea4ee55a232d08d0
Nathan <brain.nathan@gmail.com>
2015-05-25 23:53:28 -0700

    More practice with formatting and Repr()

---
+++ ex8.py
@@ -0,0 +1,14 @@
+# Variable assigned to a string of format things
+formatter = "%r %r %r %r"
+
+# Shows how you can inject format into variables
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+	"I had this thing.",
+	"That you could type up right.",
+	"But it didn't sing.",
+	"So I said goodnight."
+)
