cbe8a553c515917ada7f3950c9b2073096786d85
Nathan <brain.nathan@gmail.com>
2015-05-30 11:03:13 -0700

    Practice with functions

--- ex19.sd3.py
+++ ex19.sd3.py
@@ -1,10 +1,15 @@
 def add2(num1, num2):
-	print num1 + num2
+	charlie(num1, num2)
 	
-add2(2, 5)
+def charlie(num1, num2):
+	write = str(num1 + num2)
+	file = raw_input("What is your least favourite number?") + ".txt"
+	open(file, 'w').write(write)
+	
 
 num1 = int(raw_input("Give me a number: "))
 num2 = int(raw_input("Give me another number:"))
 
 add2(num1, num2)
+
 
