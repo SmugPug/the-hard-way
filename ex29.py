5ec89898cfa2f43d237784d68c77a2e4cefd38d7
Nathan <brain.nathan@gmail.com>
2015-05-30 15:06:09 -0700

    Intro to if statements and boolean evaluation

---
+++ ex29.py
@@ -0,0 +1,27 @@
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+	print "Too many cats!"
+	
+if people > cats:
+	print "Not enough cats!"
+
+if people < dogs:
+	print "NOT ENOUGH DAWWGS!"
+
+if people > dogs:
+	print "MORE DOGS PLEASE!"
+	
+dogs += 5
+
+if people >= dogs:
+	print "People are greater than or equal to dogs."
+	
+if people <= dogs:
+	print "People are less than or equal to dogs."
+	
+if people == dogs:
+	print "People are dogs"
